6266a2daa6e2783a7b5b300eff9b3bb88502cd20
yabeselkana <yabeszega1997@gmail.com>
2023-11-15 23:17:27 +0700

    new repo

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis blanditiis nulla quas ipsam at delectus animi cumque voluptate suscipit doloribus distinctio quis quo laudantium modi perferendis tempore culpa odio officia itaque
+        voluptatem illum, ratione atque ut? Ullam dolorem perspiciatis excepturi rem magni sequi, aut vero ipsam voluptate labore adipisci voluptatum laboriosam quisquam quod quia quidem praesentium pariatur, cupiditate molestiae ex atque
+        reiciendis deserunt. Cupiditate architecto dicta animi quasi! Expedita itaque voluptatum omnis totam aliquam culpa eaque nobis voluptate perferendis, earum cupiditate necessitatibus exercitationem possimus commodi error ab, ipsam
+        atque fugit ad enim, sequi maiores saepe! Totam vel consequuntur obcaecati animi?
+      </p>
+    </div>
+  );
+};
+
+export default Home;
